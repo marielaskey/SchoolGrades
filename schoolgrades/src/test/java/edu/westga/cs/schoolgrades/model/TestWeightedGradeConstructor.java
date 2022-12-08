0a6bfc03828340a85d8cfe8e4f728220a6b5dca7
Lauramarie Laskey <ll00098@my.westga.edu>
2022-12-07 20:19:37 -0500

    Changes to TestCompositeGradeConstructor test class

--- schoolgrades/src/test/java/edu/westga/cs/schoolgrades/model/TestWeightedGradeConstructor.java
+++ schoolgrades/src/test/java/edu/westga/cs/schoolgrades/model/TestWeightedGradeConstructor.java
@@ -3,7 +3,6 @@ package edu.westga.cs.schoolgrades.model;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestWeightedGradeConstructor {
